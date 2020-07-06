f464b9fb9debf895dc1e5178bcffc0349449975d
Алексей Гагарин <alex4619748@rambler.ru>
2020-07-06 15:24:02 +0300

    refactoring

--- Accounting.Model/EntityLibrary/Entityes/Users/ProgrammUser.cs
+++ Accounting.Model/EntityLibrary/Entityes/Users/ProgrammUser.cs
@@ -2,7 +2,7 @@
 
 namespace Accounting.Model.EntityLibrary.Entityes.Users
 {
-    class ProgrammUser : IEntity, IUser
+    public class ProgrammUser : IEntity, IUser
     {
         public int Id { get; set; }
         public string DisplayName { get; set; }
