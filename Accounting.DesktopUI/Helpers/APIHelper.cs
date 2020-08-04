ab5f6d84eaa7aa64e2ba6f76a03ec0f773534f37
Алексей Гагарин <alex4619748@rambler.ru>
2020-08-04 12:34:43 +0300

    Add APIHelper

--- Accounting.DesktopUI/Helpers/APIHelper.cs
+++ Accounting.DesktopUI/Helpers/APIHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -18,9 +19,31 @@ namespace Accounting.DesktopUI.Helpers
         }
         private void InitializeClient()
         {
+
+            string api = ConfigurationManager.AppSettings["api"];
+
             ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri(api);
             ApiClient.DefaultRequestHeaders.Accept.Clear();
             ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task Authenticate(string userName, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("UserName", userName),
+                new KeyValuePair<string, string>("password", password)
+            });
+
+            using (HttpResponseMessage response = await ApiClient.PostAsync("/Token", data))
+            {
+                if(response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<string>();
+                }
+            }
         }
     }
 }
